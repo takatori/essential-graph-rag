9192ec9a3cd1f8d23def2313ac63bfb72d32d805
takatori <takatorix@gmail.com>
2025-07-30 10:08:59 +0900

    ch03追加

---
+++ README.md
@@ -0,0 +1,7 @@
+Essential GraphRAG
+---
+
+このリポジトリは[Essential GraphRAG](https://neo4j.com/essential-graphrag/)を実際にやってみたものです。
+
+
+
---
+++ utils.py
@@ -0,0 +1,65 @@
+from neo4j import GraphDatabase
+from sentence_transformers import SentenceTransformer
+
+neo4j_driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "password"))
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def chunk_text(text, chunk_size, overlap, split_on_whitespace_only=True):
+    chunks = []
+    index = 0
+
+    while index < len(text):
+        if split_on_whitespace_only:
+            prev_whitespace = 0
+            left_index = index - overlap
+            while left_index >= 0:
+                if text[left_index] == " ":
+                    prev_whitespace = left_index
+                    break
+                left_index -= 1
+            next_whitespace = text.find(" ", index + chunk_size)
+            if next_whitespace == -1:
+                next_whitespace = len(text)
+            chunk = text[prev_whitespace:next_whitespace].strip()
+            chunks.append(chunk)
+            index = next_whitespace + 1
+        else:
+            start = max(0, index - overlap + 1)
+            end = min(index + chunk_size + overlap, len(text))
+            chunk = text[start:end].strip()
+            chunks.append(chunk)
+            index += chunk_size
+
+    return chunks
+
+def embed(texts):
+    embeddings = embedding_model.encode(texts, convert_to_tensor=True)
+    return embeddings
+
+def chunk_text(text, chunk_size,overlap, split_on_whitespace_only=True):
+    chunks = []
+    index = 0
+
+    while index < len(text):
+        if split_on_whitespace_only:
+            prev_whitespace = 0
+            left_index = index - overlap
+            while left_index >= 0:
+                if text[left_index] == " ":
+                    prev_whitespace = left_index
+                    break
+                left_index -= 1
+            next_whitespace = text.find(" ", index + chunk_size)
+            if next_whitespace == -1:
+                next_whitespace = len(text)
+            chunk = text[prev_whitespace:next_whitespace].strip()
+            chunks.append(chunk)
+            index = next_whitespace + 1
+        else:
+            start = max(0, index - overlap + 1)
+            end = min(index + chunk_size + overlap, len(text))
+            chunk = text[start:end].strip()
+            chunks.append(chunk)
+            index += chunk_size
+
+    return chunks
